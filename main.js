4a04856d1fbc771d7526c7c84ee5b6c72f806c77
ilyass daafane <ilyassdaa11@gmail.com>
2023-01-21 15:24:23 +0100

    added a current script to the navigation bar

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+//nav-bar
+const logo = document.querySelector('.logo')
+const home = document.querySelector('.home');
+const champions = document.querySelector('.champions');
+const about = document.querySelector('.about');
+
+logo.addEventListener('click', function() {
+    home.classList.add('current');
+    champions.classList.remove('current');
+    about.classList.remove('current');
+});
+home.addEventListener('click', function() {
+    home.classList.add('current');
+    champions.classList.remove('current');
+    about.classList.remove('current');
+});
+champions.addEventListener('click', () => {
+    home.classList.remove('current');
+    champions.classList.add('current');
+    about.classList.remove('current');
+});
+about.addEventListener('click', () => {
+    home.classList.remove('current');
+    champions.classList.remove('current');
+    about.classList.add('current');
+});
+// hero-section
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const container = document.querySelector('.container');
